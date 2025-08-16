e47f03cff9e632696405f23d9aa8d508c542a2c3
서민재 <hey_minj@naver.com>
2025-08-16 20:29:53 +0900

    urls 설정 및 sendpacket

--- config/urls.py
+++ config/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("game/", include("game")),
-    path("rooms/", include("game")),
+    path("rooms/", include("rooms")),
+    path("packet/", include("packet")),
 ]
